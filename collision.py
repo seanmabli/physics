90394619c74d4193bed9de167b51f1beeb32449b
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-03-08 15:28:51 +0000

    simplify with function, remove matpylib

--- collision.py
+++ collision.py
@@ -1,39 +1,25 @@
-# a solution to 3b1b collision pi video
+def twoobject(amass, a_velocity, bmass, b_velocity):
+  final_b_velocity = (2 * (a_velocity * a_mass) + b_velocity * (b_mass - a_mass)) / (a_mass + b_mass)
+  final_a_velocity = (b_velocity - a_velocity + final_b_velocity)
+  return final_a_velocity, final_b_velocity
 
-import copy
-import matplotlib.pyplot as plt
+a_mass = 1 # kg
+b_mass = 100 ** 2 # kg
 
-colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1), (0, 1, 1)]
+a_velocity = 0 # m/sec
+b_velocity = -1 # m/sec
 
-for w in range(0, 3):
-  a_mass = 1 # kg
-  b_mass = 100 ** w # kg
+collisions = 0
+step = 0
+num = 0
 
-  initial_a_velocity = 0 # m/sec
-  initial_b_velocity = -1 # m/sec
+while a_velocity > b_velocity or step < 2:
+  a_velocity, b_velocity = twoobject(a_mass, a_velocity, b_mass, b_velocity)
 
-  collisions = 0
-  step = 0
-  num = 0
-
-  while initial_a_velocity > initial_b_velocity or step < 2:
-    final_b_velocity = (2 * (initial_a_velocity * a_mass) + initial_b_velocity * (b_mass - a_mass)) / (a_mass + b_mass)
-    final_a_velocity = (initial_b_velocity - initial_a_velocity + final_b_velocity)
-
-    initial_a_velocity = copy.copy(final_a_velocity)
-    initial_b_velocity = copy.copy(final_b_velocity)
-
-    if initial_a_velocity < 0:
-      initial_a_velocity = abs(initial_a_velocity)
-      collisions += 1
-
-      num += 1
-      plt.scatter(num, initial_b_velocity, color=colors[w], s=1)
-
-    num += 1
-    plt.scatter(num, initial_b_velocity, color=colors[w], s=1)
-
+  if a_velocity < 0:
+    a_velocity = abs(a_velocity)
     collisions += 1
-    step += 1
 
-  plt.show()
+  collisions += 1
+
+print(collisions)
